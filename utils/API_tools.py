143efbf25b3adaf0f9c6583f5d25a010d0519b1d
Alexander Lundqvist <aleaxander.lundqvist@nordomatic.com>
2024-09-14 18:15:30 +0200

    Dark theme

--- utils/API_tools.py
+++ utils/API_tools.py
@@ -1,21 +1,23 @@
-from secret import API_KEY
-import requests
 import datetime
+import requests 
+from secret import API_KEY
 
-# Hämta dagens datum
-today = datetime.datetime.utcnow()
-
-# Beräkna 30 dagar framåt
-end_date = today + datetime.timedelta(days=30)
-
-# Din Long-lived access token för autentisering
-headers = {
-    "Authorization": f"Bearer {API_KEY}",
-    "content-type": "application/json",
-}
 
 def fetchCalendar(): 
+    # Hämta dagens datum
+    today = datetime.datetime.utcnow()
+
+    # Beräkna 30 dagar framåt
+    end_date = today + datetime.timedelta(days=30)
+    
     url = "http://192.168.50.11:8123/api/calendars/calendar.hanna_alex_bastisar"
+
+
+    # Din Long-lived access token för autentisering
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "content-type": "application/json",
+    }
 
     # Använd dagens datum och 30 dagar framåt
     params = {
@@ -24,12 +26,12 @@ def fetchCalendar():
     }
 
     # Skicka GET-begäran
-    response = requests.get(url, headers=headers, params=params) 
+    response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
         events = response.json()
         
-        # Strukturera datan
+        # Strukturera data
         structured_data = {}
         for event in events:
             # Extrahera datum (dagens datum)
@@ -38,10 +40,15 @@ def fetchCalendar():
             else:
                 event_date = event['start']['date']  # För heldagshändelser
 
-            # Lägg till händelser till rätt dag
+            # Beräkna veckodagen
+            event_date_obj = datetime.datetime.strptime(event_date, "%Y-%m-%d")
+            day_of_week = event_date_obj.strftime("%A")  # Exempel: "Monday"
+            
+            # Lägg till händelsen under rätt dag och datum
             if event_date not in structured_data:
-                structured_data[event_date] = []
-            structured_data[event_date].append(event['summary'])
+                structured_data[event_date] = {'tasks': [], 'day_of_week': day_of_week}
+            
+            structured_data[event_date]['tasks'].append(event['summary'])
         
         return structured_data
     else:
