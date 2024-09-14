04808e6277f9a34506fa96b44d6af1465e2d0e1a
Alexander Lundqvist <aleaxander.lundqvist@nordomatic.com>
2024-09-14 19:59:15 +0200

    colums

--- utils/API_tools.py
+++ utils/API_tools.py
@@ -4,14 +4,13 @@ from secret import API_KEY
 
 
 def fetchCalendar(): 
+    url = "http://192.168.50.11:8123/api/calendars/calendar.hanna_alex_bastisar"
+
     # Hämta dagens datum
     today = datetime.datetime.utcnow()
 
     # Beräkna 30 dagar framåt
-    end_date = today + datetime.timedelta(days=30)
-    
-    url = "http://192.168.50.11:8123/api/calendars/calendar.hanna_alex_bastisar"
-
+    end_date = today + datetime.timedelta(days=7)
 
     # Din Long-lived access token för autentisering
     headers = {
@@ -21,7 +20,7 @@ def fetchCalendar():
 
     # Använd dagens datum och 30 dagar framåt
     params = {
-        "start": today.strftime("%Y-%m-%dT%H:%M:%SZ"),  # Format till ISO 8601
+        "start": today.strftime("%Y-%m-%dT%H:%M:%SZ"),
         "end": end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
     }
 
@@ -34,21 +33,24 @@ def fetchCalendar():
         # Strukturera data
         structured_data = {}
         for event in events:
-            # Extrahera datum (dagens datum)
+            # Extrahera datum och tid
             if 'dateTime' in event['start']:
                 event_date = event['start']['dateTime'][:10]  # YYYY-MM-DD format
+                event_time = event['start']['dateTime'][11:16]  # HH:MM format
             else:
                 event_date = event['start']['date']  # För heldagshändelser
+                event_time = "Heldag"
 
             # Beräkna veckodagen
             event_date_obj = datetime.datetime.strptime(event_date, "%Y-%m-%d")
-            day_of_week = event_date_obj.strftime("%A")  # Exempel: "Monday"
-            
+            day_of_week = event_date_obj.strftime("%A")
+
             # Lägg till händelsen under rätt dag och datum
             if event_date not in structured_data:
                 structured_data[event_date] = {'tasks': [], 'day_of_week': day_of_week}
-            
-            structured_data[event_date]['tasks'].append(event['summary'])
+
+            # Lägg till både tid och beskrivning
+            structured_data[event_date]['tasks'].append(f"{event_time} - {event['summary']}")
         
         return structured_data
     else:
